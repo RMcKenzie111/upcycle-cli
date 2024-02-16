f96c206685e5c6f228b22c3530a1a6a5c2853cae
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-02-16 19:23:33 +0000

    test folder and first file

---
+++ upcycle.rb
@@ -0,0 +1,13 @@
+# Program that takes user input to determine if fabric gets upcycled or goes to a landfill
+
+# Prompt that tells user they're speaaking to a clothing item and what the item is 
+puts "Enter first text:"
+text1 = gets.chomp
+
+# Prompt that gives user a choice to upcycle the item into something else or throw it away
+
+# If the user decides to upcycle the user is prompted with basic questions (favorite color, etc)
+  #  together the answers decide what the item is turned into 
+  # the user is given info about the waste saved from preenting the item from going to a landfill
+
+# If the user decides to send the item to the landfill they're told the harm caused by that action
