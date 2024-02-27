a04fcae229a66e9488519a78a04c13a2c898d463
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-02-27 01:33:55 +0000

    added items to different method

--- items.rb
+++ items.rb
@@ -11,6 +11,9 @@ class Items
 #pp item
 # Prompt that tells user they're speaaking to a clothing item and what the item is 
   def upcycle_me_please
+    items = {20 => "pants", 10 => "dress", 12 => "coat", 5 => "shirt", 7 => "shorts", 3 => "tote"} 
+    item = items.values
+    an_item = item[rand(item.size)]
     puts "Hello! Pick an item: pants, dress, coat, shirt, shorts, tote"
     user_item = gets.chomp
     puts "Do you want to upcycle it?(yes/no)"
