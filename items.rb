36bceabcdf7b1a14b7de17245478ef62bfcb0527
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-02-28 17:38:46 +0000

    changed comment

--- items.rb
+++ items.rb
@@ -1,5 +1,4 @@
-#require "./apparel.rb"
-# Program that takes user input to determine if fabric gets upcycled or goes to a landfill
+# Program that takes user input to determine if clothing item gets upcycled, goes to a landfill, or is recycled.
 class Items
   attr_accessor :upcycle, :landfill, :new_item, :recycled, :pick_item
 
@@ -10,9 +9,7 @@ class Items
     @recycled = recycled
     @pick_item = pick_item
   end
-  
-#pp item
-# Prompt that tells user they're speaaking to a clothing item and what the item is 
+   
   def upcycle_me_please
     itemss = {20 => "pants", 10 => "dress", 12 => "coat", 5 => "shirt", 7 => "shorts", 3 => "tote"} 
     item = itemss.values
@@ -27,8 +24,6 @@ class Items
       puts "Congrats! You've upcycled #{self.pick_item} into a #{self.upcycle}. It will be added to the 1% of items that are transformed each year."
     when "no"
       to_landfill()
-      #else
-       # puts "Wrong user input, please try again"
     end
   end
   
@@ -52,12 +47,3 @@ class Items
       "the #{@pick_item} into a(n) #{@recycled}!"
   end
 end
-
-
-# Prompt that gives user a choice to upcycle the item into something else or throw it away
-
-# If the user decides to upcycle the user is prompted with basic questions (favorite color, etc)
-  #  together the answers decide what the item is turned into 
-  # the user is given info about the waste saved from preenting the item from going to a landfill
-
-# If the user decides to send the item to the landfill they're told the harm caused by that action
